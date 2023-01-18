4fd59e4ed74e0ff2024dc01d5dc4b86f09fbc353
Phil Nelson <nelsonpa@miamioh.edu>
2023-01-18 16:10:30 +0900

    Added glowing hover effect animations to the useEffect demo

--- src/most-common-hooks/Effect.jsx
+++ src/most-common-hooks/Effect.jsx
@@ -7,7 +7,7 @@ export default function Effect(){
         <div>
             <h2>useEffect</h2>
             <div className="hook-wrapper effect-hook" ref={ref}>
-                {hovered ? <h1>Hovering</h1> : <h1>Not hovering</h1>}
+                {hovered ? <h1>WOWOWOW</h1> : <h3>Hover me</h3>}
             </div>
         </div>
 )}
