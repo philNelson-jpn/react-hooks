82aff31d9a67b37001e6e5961ed4f2d20a8f1cb0
Phil Nelson <nelsonpa@miamioh.edu>
2023-03-10 09:11:30 +0900

    Added custom hook useIsOnScreen

---
+++ src/custom-hooks/useIsOnscreen.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function useIsOnscreen() {
+	const [isOnscreen, setIsOnscreen] = React.useState(false)
+
+	const elementRef = React.useRef()
+
+	React.useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			const [entry] = entries
+
+			setIsOnscreen(entry.isIntersecting)
+		})
+
+		observer.observe(elementRef.current)
+
+		return () => {
+			observer.disconnect()
+		}
+	}, [elementRef])
+
+	return [isOnscreen, elementRef]
+}
+
+export default useIsOnscreen
